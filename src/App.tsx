ce422e4207b933f755bccf70ccd21d94a1e944a6
jpwilson <jpwilson@hey.com>
2023-09-03 22:19:27 -0500

    having a great time, made a button that disappeared!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 
+import { useState } from "react";
+
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        <p>Now, I can put anything here</p>
-      </Alert>
-      <Button
-        onClick={() => console.log("we clicked bros!")}
-        buttonVariant="warning"
-      >
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          <p>Now, I can put anything here</p>
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)} buttonVariant="warning">
         This is the style in the btn
       </Button>
     </div>
